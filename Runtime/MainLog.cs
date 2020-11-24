09d5e70a591f659a1c43719c0a4e9cbb49af5a53
Ivan <ivannikit16@gmail.com>
2020-11-24 10:57:13 +0300

    refactoring MainLog

--- Runtime/MainLog.cs
+++ Runtime/MainLog.cs
@@ -3,22 +3,22 @@ using JetBrains.Annotations;
 
 namespace TeamZero.Core.Logging
 {
-    public static class Main
+    public static class MainLog
     {
-        private static Log _log;
+        private static Log _instance;
 
-        public static Log Log
+        private static Log Instance
         {
             get
             {
-                if (_log == null)
+                if (_instance == null)
                 {
                     ILogTarget listener = DefaultTarget();
                     ILogFilter filter = DefaultFilter();
-                    _log = new Log(listener, filter);
+                    _instance = new Log(listener, filter);
                 }
 
-                return _log;
+                return _instance;
             }
         }
 
@@ -26,10 +26,10 @@ namespace TeamZero.Core.Logging
 
         public static void Init([NotNull] Log value)
         {
-            if (_log != null)
+            if (_instance != null)
                 throw new Exception("Default value already used");
 
-            _log = value ?? throw new ArgumentNullException(nameof(value));
+            _instance = value ?? throw new ArgumentNullException(nameof(value));
         }
 
         public static ILogTarget DefaultTarget()
@@ -43,5 +43,22 @@ namespace TeamZero.Core.Logging
 
         public static ILogFilter DefaultFilter(bool info = true, bool warning = true, bool error = true, bool exception = true) =>
             LogFilter.Create(info, warning, error, exception);
+
+        
+        public static bool InfoEnabled() => _instance.InfoEnabled();
+
+        public static bool WarningEnabled() => _instance.WarningEnabled();
+
+        public static bool ErrorEnabled() => _instance.ErrorEnabled();
+		
+        public static bool ExceptionEnabled() => _instance.ExceptionEnabled();
+		
+        public static void Info(object o) => _instance.Info(o);
+		
+        public static void Warning(object o) => _instance.Warning(o);
+		
+        public static void Error(object o) => _instance.Error(o);
+		
+        public static void Exception(Exception e) => _instance.Exception(e);
     }
 }
