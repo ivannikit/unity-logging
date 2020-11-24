06bb06266602af2bae813d883e01907dea063993
Ivan <ivannikit16@gmail.com>
2020-11-24 11:05:19 +0300

    update MainLog

--- Runtime/MainLog.cs
+++ Runtime/MainLog.cs
@@ -5,20 +5,19 @@ namespace TeamZero.Core.Logging
 {
     public static class MainLog
     {
-        private static Log _instance;
-
-        private static Log Instance
+        private static Log _target;
+        public static Log Target
         {
             get
             {
-                if (_instance == null)
+                if (_target == null)
                 {
                     ILogTarget listener = DefaultTarget();
                     ILogFilter filter = DefaultFilter();
-                    _instance = new Log(listener, filter);
+                    _target = new Log(listener, filter);
                 }
 
-                return _instance;
+                return _target;
             }
         }
 
@@ -26,10 +25,10 @@ namespace TeamZero.Core.Logging
 
         public static void Init([NotNull] Log value)
         {
-            if (_instance != null)
+            if (_target != null)
                 throw new Exception("Default value already used");
 
-            _instance = value ?? throw new ArgumentNullException(nameof(value));
+            _target = value ?? throw new ArgumentNullException(nameof(value));
         }
 
         public static ILogTarget DefaultTarget()
@@ -45,20 +44,20 @@ namespace TeamZero.Core.Logging
             LogFilter.Create(info, warning, error, exception);
 
         
-        public static bool InfoEnabled() => _instance.InfoEnabled();
+        public static bool InfoEnabled() => _target.InfoEnabled();
 
-        public static bool WarningEnabled() => _instance.WarningEnabled();
+        public static bool WarningEnabled() => _target.WarningEnabled();
 
-        public static bool ErrorEnabled() => _instance.ErrorEnabled();
+        public static bool ErrorEnabled() => _target.ErrorEnabled();
 		
-        public static bool ExceptionEnabled() => _instance.ExceptionEnabled();
+        public static bool ExceptionEnabled() => _target.ExceptionEnabled();
 		
-        public static void Info(object o) => _instance.Info(o);
+        public static void Info(object o) => _target.Info(o);
 		
-        public static void Warning(object o) => _instance.Warning(o);
+        public static void Warning(object o) => _target.Warning(o);
 		
-        public static void Error(object o) => _instance.Error(o);
+        public static void Error(object o) => _target.Error(o);
 		
-        public static void Exception(Exception e) => _instance.Exception(e);
+        public static void Exception(Exception e) => _target.Exception(e);
     }
 }
